bebb7087bb533181e02f777f2135f246f6f6be4b
Rohmad Arifin <rohmadarifin007@gmail.com>
2019-05-10 16:36:12 +0700

    show data to list component

--- src/components/List.js
+++ src/components/List.js
@@ -3,17 +3,26 @@ import { connect } from "react-redux";
 import { fetchData } from "../actions/todoAction";
 
 class List extends Component {
-  componentDidMount() {}
+  componentDidMount() {
+    this.props.fetchData();
+  }
   render() {
     return (
       <div>
-        <h1>Todo List</h1>
+        <h2>Todo List</h2>
+        {this.props.todoList.map((todo, index) => (
+          <div key={index}>{todo.description}</div>
+        ))}
       </div>
     );
   }
 }
 
+const mapStateToProps = state => ({
+  todoList: state.todo.todoList
+});
+
 export default connect(
-  null,
+  mapStateToProps,
   { fetchData }
 )(List);
